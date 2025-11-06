2d423a7e42b29a7dcd68428d2b0f2606a10369a6
Givari Awardi <givariawardi212@gmail.com>
2025-11-06 08:24:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6993845638-19a1f",
+  "appId": "1:1043203517529:web:bce992bf5e610483bad548",
+  "apiKey": "AIzaSyAKbB9yPeePaWVInNQDzCMiCC_4Fg7jHyk",
+  "authDomain": "studio-6993845638-19a1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1043203517529"
+};
